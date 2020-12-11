fd39d3037c45583423f1400feb4241abb57cb74c
Dinkar Choudhary <dinkarchoudhary21@gmail.com>
2020-12-11 17:08:31 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace FolderDeletion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program for Deletion of Folder in Windows
+            var root = ConfigurationManager.AppSettings["Path"];
+            try
+            {
+                if (Directory.Exists(root))
+                {
+                    Directory.Delete(root, true);
+                    Console.WriteLine("File Successfully Deleted");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("File Doesn't Exists");
+            }
+        }
+    }
+}
